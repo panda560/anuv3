9eec0fc31f3ced9ecb569793ea70eee3fabc1c3b
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-06-28 04:45:22 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
     "displayName": "ᴏᴡɴᴇʀ",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:mursid-bot;Owner;;;\nFN:Creator Faiz ԅ( ͒ ۝ ͒ )ᕤ\nitem1.TEL;waid=6281273677810:6281273677810\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:akunsesi021@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Lampung 🇮🇩\nitem5.X-ABLabel:───────[ PB BOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:pratama ;Owner;;;\nFN:Creator Faiz ԅ( ͒ ۝ ͒ )ᕤ\nitem1.TEL;waid=6281273677810:6281273677810\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:akunsesi021@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://hardianto.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Lampung 🇮🇩\nitem5.X-ABLabel:───────[ M.A.F.PRATAMA ]───────\nEND:VCARD"
   }, {
     "displayName": "ᴏᴡɴᴇʀ",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Run;Jasa;;;\nFN:+62 882-3383-2771\nitem1.TEL;waid=6288233832771:6288233832771\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:mursidmulo@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:donasi chat owner ya></\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia 🇮🇩\nitem5.X-ABLabel:───────[ PB BOT ]───────\nEND:VCARD"
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Run;Jasa;;;\nFN:+62 819-5929-3465\nitem1.TEL;waid=6281959293465:6281959293465\nitem1.X-ABLabel:📍 Owner\nitem2.EMAIL;type=INTERNET:mursidmulo@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:donasi chat owner ya></\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Indonesia 🇮🇩\nitem5.X-ABLabel:───────[ ANGGRA ]───────\nEND:VCARD"
   }]
   }, MessageType.contactsArray, { quoted: m })
 }
